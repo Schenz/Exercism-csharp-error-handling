025e0933180348f6d5481fbdcdd79ec899f99fdc
Schenz <bschenz@gmail.com>
2018-08-10 15:51:23 -0400

    format code

--- ErrorHandling.cs
+++ ErrorHandling.cs
@@ -7,9 +7,12 @@ public static class ErrorHandling
     public static int? HandleErrorByReturningNullableType(string input)
     {
         int result;
-        if (int.TryParse(input, out result)) {
+        if (int.TryParse(input, out result))
+        {
             return result;
-        } else {
+        }
+        else
+        {
             return null;
         }
     }
@@ -18,10 +21,12 @@ public static class ErrorHandling
 
     public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
     {
-        try {
+        try
+        {
             throw new Exception();
         }
-        finally {
+        finally
+        {
             disposableObject.Dispose();
         }
     }
